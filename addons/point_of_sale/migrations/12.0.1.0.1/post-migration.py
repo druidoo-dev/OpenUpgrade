85bc2ea5faab325b8f03b4ae56123f6d9cdb9b61
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-06-02 18:12:24 +0200

    Process in batches

--- addons/point_of_sale/migrations/12.0.1.0.1/post-migration.py
+++ addons/point_of_sale/migrations/12.0.1.0.1/post-migration.py
@@ -1,11 +1,19 @@
 # Copyright 2019 Eficent <http://www.eficent.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
+from odoo import models
 
 
 def fill_pos_order_amounts(env):
     """Avoid null values in required fields."""
-    env["pos.order"].search([])._compute_batch_amount_all()
+    orders = env["pos.order"].search([])
+    batches = [
+        orders[i:i + models.PREFETCH_MAX]
+        for i in range(0, len(orders), models.PREFETCH_MAX)
+    ]
+    for batch in batches:
+        batch._compute_batch_amount_all()
+        batch._prefetch = dict()
 
 
 def fill_pos_order_line_prices(env):
